f8b5b1b93adf277b43def5bcbe3d80cd567054b1
saurabh gupta <saurabhgup98@gmail.com>
2021-08-12 12:48:22 +0530

    First Commit

---
+++ src/defaultPackage/test1.java
@@ -0,0 +1,15 @@
+package defaultPackage;
+
+public class test1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String hungry = "yes";
+		if (hungry == "yes") {
+		System.out.println("doing correct");
+		} else {
+			System.out.println("Doing Nothing");
+		}
+	}
+
+}
